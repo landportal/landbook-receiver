2d2a33ea330c3c42b988ba0ce8b6a3c5d7ffaf9a
belaustegui <cristian.alvb@gmail.com>
2014-03-17 20:40:06 +0100

    The file to parse is now received from a POST request. Closes #7

--- app/services.py
+++ app/services.py
@@ -4,13 +4,16 @@ from model import models
 
 class ParserService(object):
 
-    def __init__(self):
-        self.tm = TransactionManager()
+    def parse_nodes_content(self, content):
+        """Parse a XML file and return the corresponding model.
 
-    def parse_nodes_content(self):
+        Parse a XML (the XML file must conform with the Landportal schema)
+        to create an object representation based on the Landportal-model.
+        Returns the root of the model-tree (the Dataset).
+        """
         import xml.etree.ElementTree as Et
-        tree = Et.parse('xml/example_xml_ipfri_no_slices.xml')
-        dataset = self._parse_dataset(tree.getroot())
+        root_node = Et.fromstring(content)
+        dataset = self._parse_dataset(root_node)
         return dataset
 
     def _parse_dataset(self, node):
@@ -20,10 +23,19 @@ class ParserService(object):
         dataset.datasource = datasource
         license = self._parse_license(node.find('license'))
         dataset.license = license
+        # Dataset indicators
+        # indicators = {}
+        # for item in node.find('indicators').findall('indicator'):
+        #     ind = self._parse_indicator(item, dataset)
+        #     indicators[ind.id_source] = ind
+        # Dataset compound indicators
+        #comp_indicators = []
+        #for item in node.find('indicators').findall('compound_indicator'):
+        #    comp_indicators.append(self._parse_compound_indicator(item, dataset, indicators))
+        #return dataset
         # Dataset observations
-        #indicators = []
-        #for item in node.find('indicators').findall('indicator'):
-        #    indicators.append(self._parse_indicator(item, dataset))
+        #for item in node.find('observations').findall('observation'):
+        #    self._parse_observation(item, dataset, indicators)
         return dataset
 
     def _parse_datasource(self, node):
@@ -40,35 +52,63 @@ class ParserService(object):
         return license
 
     def _parse_indicator(self, node, dataset):
-        indicator = models.Indicator(id_source=node.get('id'))
-        indicator.dataset = dataset
-        indicator.name = node.find('ind_name').text
-        indicator.description = node.find('ind_description').text
+        """Parse an indicator node and return an Indicator object."""
+        id_source = node.get('id')
+        name = node.find('ind_name').text
+        description = node.find('ind_description').text
         measurement = self._parse_measurement(node.find('measure_unit'))
+
+        indicator = models.Indicator(id_source=id_source,
+                                     name=name,
+                                     description=description,
+                                     )
         indicator.measurement_unit = measurement
+        indicator.dataset = dataset
         return indicator
 
     def _parse_measurement(self, node):
+        """Parse a measurement node and return a MeasurementUnit object."""
         measurement = models.MeasurementUnit(name=node.text)
         return measurement
 
+    def _parse_compound_indicator(self, node, dataset, indicators):
+        """Parse a compound indicator node and return a CompoundIndicator object."""
+        id_source = node.get('id')
+        name = node.find('ind_name').text
+        description = node.find('ind_description').text
+        measurement = self._parse_measurement(node.find('measure_unit'))
 
-class TransactionManager(object):
-    '''
-    Transaction manager that helps to abstract from the execution
+        indicator = models.CompoundIndicator(id_source=id_source,
+                                             name=name,
+                                             description=description,
+                                             )
+        indicator.measurement_unit = measurement
+        indicator.dataset = dataset
+        for rel in node.findall('indicator-ref'):
+            # Buscar el indicador que sea en la coleccion de indicadores y
+            # establecer en dicho indicador que esta relacionado con este
+            print rel.get('id')
+            indicators[rel.get('id')].compound_indicator = indicator
+        return indicator
 
-    @author: Herminio
-    '''
+    def _parse_observation(self, node, dataset, indicators):
+        id_source = node.get('id')
+        # TODO: parse issued
+        obs_status = node.find('obs_status').text
+        # TODO: parse value
+        # TODO: parse computation
+        # TODO: parse country
+        # TODO: parse time (ref-time)
+        # TODO: parse indicator group
+        # TODO: parse region
+        # TODO: parse slice
+        rel_indicator_id = node.find('indicator-ref').get('id')
 
-    def execute(self, dao, function, *args):
-        '''
-        Abstraction for all calls to the dao methods, like command executor
-        '''
-        session = db.session
-        getattr(dao, 'set_session')(session)
-        result = function(*args)
-        session.commit()
+        # Meter los atributos que sean
+        observation = models.Observation()
+        observation.indicator = indicators[rel_indicator_id]
+        observation.dataset = dataset
 
-        return result
+        return observation
 
 
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, request, abort
 from app import app, db
 from app.services import ParserService
 
@@ -9,10 +9,21 @@ service = ParserService()
 
 class CountryListAPI(Resource):
 
-    def get(self):
-        nodes = service.parse_nodes_content()
-        session = db.session
-        session.add(nodes)
-        session.commit()
+    def post(self):
+        """ Parse an XML and store the model mapping into the database.
+
+        Receives an xml=... with the XML content to parse
+        Returns a 200 response if everything went right or 400 if there
+        is not any content to parse.
+        """
+        if 'xml' in request.form:
+            file = request.form['xml']
+            nodes = service.parse_nodes_content(file)
+            session = db.session
+            session.merge(nodes)
+            session.commit()
+        else:
+            abort(400)
+
 
 api.add_resource(CountryListAPI, '/receiver')
