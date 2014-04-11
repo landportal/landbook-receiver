20a6dd9f1860314b44553c2fa5bdf7464c071554
belaustegui <cristian.alvb@gmail.com>
2014-04-11 11:39:11 +0200

    Make the Receiver log its errors.

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,16 @@ Created on 03/02/2014
 '''
 from flask.app import Flask
 from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging import FileHandler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/landportal'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
 db = SQLAlchemy(app)
+if not app.debug:
+	file_handler = FileHandler(filename='receiver_log')
+	file_handler.setLevel(logging.WARNING)
+	app.logger.addHandler(file_handler)
 
 from app import views
--- run.py
+++ run.py
@@ -4,5 +4,5 @@ if __name__ == '__main__':
     #First create the DataBase
     #create_db.create_database()
     #Run the app
-    app.run(debug=True)
+    app.run()
 
