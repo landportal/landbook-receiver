ca50ab188c110ccece3f5d5f0de2dcc7876b0bb8
belaustegui <cristian.alvb@gmail.com>
2014-04-25 10:24:38 +0200

    Close #49

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/landportal'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
 # Set debug mode so Apache can log the errors
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
