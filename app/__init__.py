e26dbd3b1e2beb700098abe282af49dc4078d69e
belaustegui <cristian.alvb@gmail.com>
2014-05-01 19:43:36 +0200

    Close #54
    The refactor has resulted in a notable disminution in the execution
    time and in a minor reduction in the memory consumption.

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,8 @@ from flask.app import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/landportal'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/landportal'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///foo.db'
 # Set debug mode so Apache can log the errors
 app.config['DEBUG'] = True
 db = SQLAlchemy(app)
--- app/services.py
+++ app/services.py
@@ -2,76 +2,70 @@ import app
 import model.models as model
 import datetime
 from parser import Parser
-from memory_profiler import profile
 
 
 class ReceiverSQLService(object):
     def __init__(self, content):
-        parser = Parser(content)
-        self.metadata_serv = MetadataHelper(parser)
-        self.indicator_serv = IndicatorHelper(parser)
-        self.observation_serv = ObservationHelper(parser)
-        self.slice_serv = SliceHelper(parser)
+        self.parser = Parser(content)
+        self.time = datetime.datetime.now()
 
     def store_data(self, user_ip):
         session = app.db.session
         try:
             self._store_data(user_ip, session)
-            #session.commit()
+            session.commit()
         except:
             session.rollback()
             raise
         finally:
             session.close()
 
-    @profile
     def _store_data(self, user_ip, session):
         dataset = self._store_metadata(user_ip, session)
         session.flush()
         # Store indicators
         self._store_simple_indicators(dataset, session)
-        session.flush()
         self._store_compound_indicators(dataset, session)
-        session.flush()
         self._store_indicator_relationships(session)
-        session.flush()
         self._store_indicator_groups(session)
-        session.flush()
-        session.expunge_all()
         # Store observations
         self._store_observations(dataset, session)
-        session.flush()
-        session.expunge_all()
         # Store slices
         self._store_slices(dataset, session)
-        session.flush()
-
-    def _store_slices(self, dataset, session):
-        # The observation_ids list was created in the parser and WILL NOT be
-        # persisted. The list is only used here to link with the observations
-        for sli in self.slice_serv.get_slices():
-            sli.dataset_id = dataset.id
-            for rel_obs in session.query(model.Observation)\
-                    .filter(model.Observation.id.in_(sli.observation_ids)).all():
-                rel_obs.slice_id = sli.id
-            session.add(sli)
-
-    def _store_indicator_groups(self, session):
-        groups = self.indicator_serv.get_indicator_groups()
-        for group in groups:
-            indicator_ref = session.query(model.CompoundIndicator)\
-                .filter(model.CompoundIndicator.id == group.indicator_ref)\
-                .first()
-            indicator_ref.indicator_ref_group_id = group.id
-            session.merge(group)
 
     def _store_simple_indicators(self, dataset, session):
-        for item in self.indicator_serv.get_simple_indicators():
-            ind = session.merge(item)
-            dataset.add_indicator(ind)
+        def enrich_indicator(ind):
+            ind.starred = DBHelper.check_indicator_starred(session, ind.id)
+            ind.last_update = self.time
+            return ind
+
+        for item in self.parser.get_simple_indicators():
+            indicator = session.merge(enrich_indicator(item))
+            dataset.add_indicator(indicator)
+        session.flush()
+
+    def _store_compound_indicators(self, dataset, session):
+        def enrich_indicator(ind):
+            ind.starred = DBHelper.check_indicator_starred(session, ind.id)
+            ind.last_update = self.time
+            for rel in DBHelper.find_indicators(session, ind.related_id):
+                rel.compound_indicator_id = ind.id
+            return ind
+
+        for item in self.parser.get_compound_indicators():
+            indicator = session.merge(enrich_indicator(item))
+            dataset.add_indicator(indicator)
+        session.flush()
+
+    def _store_indicator_groups(self, session):
+        for group in self.parser.get_indicator_groups():
+            indicator_ref = DBHelper.find_compound_indicator(session, group.indicator_ref)
+            indicator_ref.indicator_ref_group_id = group.id
+            session.merge(group)
+        session.flush()
 
     def _store_indicator_relationships(self, session):
-        indicators = self.indicator_serv.get_simple_indicators()
+        indicators = self.parser.get_simple_indicators()
         for ind in indicators:
             # Each indicator may be related with others
             # The related_id field was created in the parser and WILL NOT be
@@ -83,140 +77,71 @@ class ReceiverSQLService(object):
                 rel.target_id = rel_id
                 relationships.append(rel)
             session.add_all(relationships)
-
-    def _store_compound_indicators(self, dataset, session):
-        for item in self.indicator_serv.get_compound_indicators():
-            ind = session.merge(item)
-            dataset.add_indicator(ind)
-            for related in session.query(model.Indicator)\
-                    .filter(model.Indicator.id.in_(item.related_id)).all():
-                related.compound_indicator_id = ind.id
+        session.flush()
 
     def _store_metadata(self, user_ip, session):
-        datasource = self.metadata_serv.get_datasource()
-        dataset = self.metadata_serv.get_dataset()
-        organization = self.metadata_serv.get_organization()
-        user = self.metadata_serv.get_user(ip=user_ip)
+        datasource = self.parser.get_datasource()
+        dataset = self.parser.get_dataset()
+        organization = self.parser.get_organization()
+        user = self.parser.get_user()
+        user.ip = user_ip
         user.organization = organization
         datasource.organization = organization
         dataset.datasource = datasource
         return session.merge(dataset)
 
-    @profile
     def _store_observations(self, dataset, session):
-        for obs in self.observation_serv.get_observations():
+        def enrich_observation(obs):
             obs.dataset_id = dataset.id
-            session.add(obs)
+            if obs.region_code is not None:
+                obs.region_id = DBHelper.find_region_id(session, obs.region_code)
+            elif obs.country_code is not None:
+                obs.region_id = DBHelper.find_country_id(session, obs.country_code)
+            return obs
 
+        for observation in self.parser.get_observations():
+            session.add(enrich_observation(observation))
+        session.flush()
+        session.expunge_all()
 
-class IndicatorHelper(object):
-    def __init__(self, parser):
-        self._parser = parser
-        self._time = datetime.datetime.now()
-        self._dbhelper = DBHelper()
+    def _store_slices(self, dataset, session):
+        def enrich_slice(sli):
+            sli.dataset_id = dataset.id
+            for rel_obs in DBHelper.find_observations(session, sli.observation_ids):
+                rel_obs.slice_id = sli.id
+            if sli.region_code is not None:
+                sli.dimension_id = DBHelper.find_region_id(session, sli.region_code)
+            elif sli.country_code is not None:
+                sli.dimension_id = DBHelper.find_country_id(session, sli.country_code)
+            return sli
 
-    def get_simple_indicators(self):
-        #indicators = self._parser.get_simple_indicators()
-        #Enrich the indicators data querying the API to get its starred state
-        #and setting the last_update field
-        return (self._enrich_indicator(ind) for ind
-                in self._parser.get_simple_indicators())
-
-    def _enrich_indicator(self, ind):
-        ind.starred = self._dbhelper.check_indicator_starred(ind.id)
-        ind.last_update = self._time
-        return ind
-
-    def get_compound_indicators(self):
-        return (self._enrich_indicator(ind) for ind
-                in self._parser.get_compound_indicators())
-
-    def get_indicator_groups(self):
-        return self._parser.get_indicator_groups()
-
-
-class ObservationHelper(object):
-    def __init__(self, parser):
-        self._parser = parser
-        self._dbhelper = DBHelper()
-
-    @profile
-    def get_observations(self):
-        return (self._enrich_observation(obs) for obs
-                in self._parser.get_observations())
-
-    def _enrich_observation(self, obs):
-        if obs.region_code is not None:
-            obs.region_id = self._dbhelper.find_region_id(obs.region_code)
-        elif obs.country_code is not None:
-            obs.region_id = self._dbhelper.find_country_id(obs.country_code)
-        return obs
-
-
-class MetadataHelper(object):
-    """Provides access to the dataset metadata
-    """
-    def __init__(self, parser):
-        self._parser = parser
-        self._dbhelper = DBHelper()
-
-    def get_user(self, ip):
-        user = self._parser.get_user()
-        user.ip = ip
-        return user
-
-    def get_organization(self):
-        return self._parser.get_organization()
-
-    def get_datasource(self):
-        datasource = self._parser.get_datasource()
-        # Check if the datasource already exists in the database
-        other = self._dbhelper.check_datasource(datasource.name)
-        return other if other is not None else datasource
-
-    def get_dataset(self):
-        return self._parser.get_dataset()
-
-
-class SliceHelper(object):
-    def __init__(self, parser):
-        self._parser = parser
-        self._dbhelper = DBHelper()
-
-    def get_slices(self):
-        # Enrich the slices linking the to its correspoding dimension. If the
-        # dimension is a Time, it comes already linked from the parser.
-        return (self._enrich_slice(sli) for sli in self._parser.get_slices())
-
-    def _enrich_slice(self, sli):
-        if sli.region_code is not None:
-            sli.dimension_id = self._dbhelper.find_region_id(sli.region_code)
-        elif sli.country_code is not None:
-            sli.dimension_id = self._dbhelper.find_country_id(sli.country_code)
-        return sli
+        for slice in self.parser.get_slices():
+            session.add(enrich_slice(slice))
+        session.flush()
+        session.expunge_all()
 
 
 class DBHelper(object):
-    def __init__(self):
-        self.session = app.db.session
-
-    def check_datasource(self, datasource_name):
+    @staticmethod
+    def check_datasource(session, datasource_name):
         """Find a DataSource by name in the DB. Returns None if not found."""
-        datasource = self.session.query(model.DataSource)\
+        datasource = session.query(model.DataSource)\
             .filter(model.DataSource.name == datasource_name)\
             .first()
         return datasource
 
-    def check_indicator_starred(self, indicator_id):
+    @staticmethod
+    def check_indicator_starred(session, indicator_id):
         """Check if an Indicator is starred"""
-        indicator = self.session.query(model.Indicator)\
+        indicator = session.query(model.Indicator)\
             .filter(model.Indicator.id == indicator_id)\
             .first()
         return indicator.starred if indicator is not None else False
 
-    def find_region_id(self, reg_code):
+    @staticmethod
+    def find_region_id(session, reg_code):
         """Get the Region ID using its UN_CODE"""
-        region = self.session.query(model.Region)\
+        region = session.query(model.Region)\
             .filter(model.Region.un_code == reg_code)\
             .first()
         if region is None:
@@ -225,9 +150,10 @@ class DBHelper(object):
         else:
             return region.id
 
-    def find_country_id(self, country_code):
+    @staticmethod
+    def find_country_id(session, country_code):
         """Get the Country ID using its ISO3"""
-        country = self.session.query(model.Country)\
+        country = session.query(model.Country)\
             .filter(model.Country.iso3 == country_code)\
             .first()
         if country is None:
@@ -235,3 +161,24 @@ class DBHelper(object):
                             "the database".format(country_code))
         else:
             return country.id
+
+    @staticmethod
+    def find_observations(session, ids):
+        """Get all observations in a list of IDs"""
+        return session.query(model.Observation)\
+            .filter(model.Observation.id.in_(ids))\
+            .all()
+
+    @staticmethod
+    def find_indicators(session, ids):
+        """Get all indicators in a list of IDs"""
+        return session.query(model.Indicator)\
+            .filter(model.Indicator.id.in_(ids))\
+            .all()
+
+    @staticmethod
+    def find_compound_indicator(session, ind_id):
+        """Get a compound indicator by its ID"""
+        return session.query(model.CompoundIndicator)\
+            .filter(model.CompoundIndicator.id == ind_id)\
+            .first()
