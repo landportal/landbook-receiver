031584b0902d2ab9783a10392e0726cae2758219
belaustegui <cristian.alvb@gmail.com>
2014-05-05 13:24:59 +0200

    Add configuration file.
    The configuration file contains the configuration variables for
    the Landportal-receiver.
    Currently it only contains the list of allowed IPs.  If this list
    is left empty ALL IPs will be allowed.

---
+++ config.py
@@ -0,0 +1,7 @@
+# LANDPORTAL-RECEIVER  CONFIGURATION  FILE
+
+# Put here all the IPs which can send requests to the receiver.
+# If this list is empty, ALL REQUESTS WILL BE ACCEPTED
+ALLOWED_IPS = [
+    "127.0.0.1",
+]
