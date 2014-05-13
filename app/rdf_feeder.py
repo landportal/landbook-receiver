0c921fa8e120b9d24fd1f77743bbcc75abff4d4e
Guillermo Infante Hernández <guillermo2104@gmail.com>
2014-05-13 18:07:27 +0200

    Refactor RDF service

--- app/rdf_feeder.py
+++ app/rdf_feeder.py
@@ -8,8 +8,8 @@ from rdf_utils.namespaces_handler import bind_namespaces
 import datetime as dt
 
 # test data
-with codecs.open(unicode('../xml/test_file.xml', encoding='utf-8')) as xml:
-        content = xml.read()
+with codecs.open(unicode('../xml/DATIPFRI_0_1_0_no_urls.xml', encoding='utf-8')) as xml:
+    content = xml.read()
 
 graph = Graph()
 logging.basicConfig()
@@ -20,26 +20,9 @@ graph_uri = "graph-uri=http://www.landportal.info"
 
 def feed_rdf():
     rdf_service = ReceiverRDFService(content)
-    rdf_service.add_observations_triples(graph)
-    rdf_service.add_indicators_triples(graph)
-    rdf_service.add_slices_triples(graph)
-    rdf_service.add_users_triples(graph)
-    rdf_service.add_organizations_triples(graph)
-    rdf_service.add_licenses_triples(graph)
-    rdf_service.add_topics_triples(graph)
-    rdf_service.add_area_triples_from_slices(graph)
-    rdf_service.add_area_triples_from_observations(graph)
-    rdf_service.add_computation_triples(graph)
-    rdf_service.add_region_triples(graph)
-    rdf_service.add_dataset_triples(graph)
-    rdf_service.add_data_source_triples(graph)
-    rdf_service.add_upload_triples(graph)
-
     bind_namespaces(graph)
-
-    rdf_service.serialize_turtle(graph)
-    rdf_service.serialize_rdf_xml(graph)
-    rdf_service.load_data_set(host, api, graph_uri)
+    #rdf_service.run_service(graph, host, api, graph_uri)
+    rdf_service._load_data_set(host=host, api=api, graph_uri=graph_uri)
 
 if __name__ == "__main__":
     start = dt.datetime.now()
--- app/rdf_service.py
+++ app/rdf_service.py
@@ -8,141 +8,136 @@ import datetime
 from countries.country_reader import CountryReader
 import datetime as dt
 import sh
+import config
+
 
 class ReceiverRDFService(object):
     def __init__(self, content):
         self.parser = Parser(content)
         self.time = datetime.datetime.now()
 
-    def add_observations_triples(self, graph):
+    def run_service(self, graph, host, api, graph_uri):
+        self._add_observations_triples(graph)
+        self._add_indicators_triples(graph)
+        self._add_area_triples_from_observations(graph)
+        self._add_area_triples_from_slices(graph)
+        self._add_computation_triples(graph)
+        self._add_data_source_triples(graph)
+        self._add_dataset_triples(graph)
+        self._add_licenses_triples(graph)
+        self._add_organizations_triples(graph)
+        self._add_region_triples(graph)
+        self._add_slices_triples(graph)
+        self._add_upload_triples(graph)
+        self._add_users_triples(graph)
+        self._add_topics_triples(graph)
+        self._serialize_rdf_xml(graph)
+        self._serialize_turtle(graph)
+        self._load_data_set(graph, host, api, graph_uri)
+        return graph
+
+    def _add_observations_triples(self, graph):
         for obs in self.parser.get_observations():
             region = self._get_area_code(obs)
-
             graph.add((prefix_.term(obs.id), RDF.type,
                        qb.term("Observation")))
-
             graph.add((prefix_.term(obs.id), cex.term("ref-time"),
                        prefix_.term(obs.ref_time.value)))
-
             graph.add((prefix_.term(obs.id), cex.term("ref-area"),
                        prefix_.term(region)))
-
             graph.add((prefix_.term(obs.id), cex.term("ref-indicator"),
                        prefix_.term(obs.indicator_id)))
-
             graph.add((prefix_.term(obs.id), cex.term("value"),
                        Literal(str(obs.value.value), datatype=XSD.double)))
-
             graph.add((prefix_.term(obs.id), cex.term("computation"),
                        cex.term(obs.computation.uri)))
-
             graph.add((prefix_.term(obs.id), dcterms.term("issued"),
                        Literal(obs.issued.timestamp, datatype=XSD.dateTime)))
-
             graph.add((prefix_.term(obs.id), qb.term("dataSet"),
                        prefix_.term(obs.dataset_id)))
-
             graph.add((prefix_.term(obs.id), qb.term("slice"),
                        prefix_.term(str(obs.slice_id))))
-
             graph.add((prefix_.term(obs.id), RDFS.label,
                        Literal("Observation of " + str(region) +
                                " within the period of " + str(obs.ref_time.value) +
                                " for indicator " + str(obs.indicator_id), lang='en')))
-
             graph.add((prefix_.term(obs.id),
                        sdmx_concept.term("obsStatus"),
                        sdmx_code.term(obs.value.obs_status)))
-
             # graph.add((prefix_.term(obs.id),
             #        lb.term("source"), prefix_.term(obs.source)))
-
         return graph
 
-    def add_indicators_triples(self, graph):
+    def _add_indicators_triples(self, graph):
         for ind in self.parser.get_simple_indicators():
             graph.add((prefix_.term(ind.id), RDF.type,
-                   cex.term("Indicator")))
-
+                       cex.term("Indicator")))
             graph.add((prefix_.term(ind.id), lb.term("preferable_tendency"),
-                   cex.term(ind.preferable_tendency)))
-
+                       cex.term(ind.preferable_tendency)))
             graph.add((prefix_.term(ind.id), lb.term("measurement"),
-                   cex.term(ind.measurement_unit.name)))
-
+                       cex.term(ind.measurement_unit.name)))
             graph.add((prefix_.term(ind.id), lb.term("last_update"),
-                   Literal(self.time, datatype=XSD.dateTime)))
-
+                       Literal(self.time, datatype=XSD.dateTime)))
             graph.add((prefix_.term(ind.id), lb.term("starred"),
-                   Literal(ind.starred, datatype=XSD.Boolean)))
-
+                       Literal(ind.starred, datatype=XSD.Boolean)))
             graph.add((prefix_.term(ind.id), lb.term("topic"),
-                   cex.term(ind.topic_id)))
-
+                       cex.term(ind.topic_id)))
             graph.add((prefix_.term(ind.id), lb.term("indicatorType"),
-                   cex.term(ind.type)))
-
+                       cex.term(ind.type)))
             graph.add((prefix_.term(ind.id), RDFS.label,
-                   Literal(ind.translations[0].name, lang='en')))
-
+                       Literal(ind.translations[0].name, lang='en')))
             graph.add((prefix_.term(ind.id), RDFS.comment,
                        Literal(ind.translations[0].description, lang='en')))
-
         return graph
 
-    def add_slices_triples(self, graph):
+    def _add_slices_triples(self, graph):
         for slc in self.parser.get_slices():
             graph.add((prefix_.term(slc.id), RDF.type,
                        qb.term("Slice")))
-
             graph.add((prefix_.term(slc.id), cex.term("indicator"),
-                      prefix_.term(str(slc.indicator_id))))
-
+                       prefix_.term(str(slc.indicator_id))))
             for obs in self.parser.get_observations():
                 if obs.slice_id == slc.id:
                     graph.add((prefix_.term(slc.id), qb.term("observation"),
                                prefix_.term(str(obs.id))))
-
             if slc.region_code is not None:
                 dimension = slc.region_code
             elif slc.country_code is not None:
                 dimension = slc.country_code
             else:
                 dimension = slc.dimension.value
-
             graph.add((prefix_.term(slc.id), lb.term("dimension"),
                        lb.term(dimension)))
-
             graph.add((prefix_.term(slc.id), qb.term("dataSet"),
-                   prefix_.term(slc.dataset_id)))
-
+                       prefix_.term(slc.dataset_id)))
         return graph
 
-    def add_users_triples(self, graph):
+    def _add_users_triples(self, graph):
         usr = self.parser.get_user()
+        organization = self.parser.get_organization()
         graph.add((prefix_.term(usr.id), RDF.type,
-               FOAF.Person))
+                   FOAF.Person))
         graph.add((prefix_.term(usr.id), RDFS.label,
-               Literal(usr.id, lang='en')))
+                   Literal(usr.id, lang='en')))
         graph.add((prefix_.term(usr.id), FOAF.name,
-               Literal(usr.id)))
+                   Literal(usr.id)))
         graph.add((prefix_.term(usr.id), FOAF.account,
-               Literal(usr.id)))
+                   Literal(usr.id)))
         graph.add((prefix_.term(usr.id), org.term("memberOf"),
-               prefix_.term(str(usr.organization_id))))
+                   URIRef(str(organization.id))))
         return graph
 
-    def add_organizations_triples(self, graph):
+    def _add_organizations_triples(self, graph):
         organization = self.parser.get_organization()
         graph.add((URIRef(organization.id), RDF.type,
-               org.term("Organization")))
+                   org.term("Organization")))
         graph.add((URIRef(organization.id), RDFS.label,
-               Literal(organization.name, lang='en')))
+                   Literal(organization.name, lang='en')))
         graph.add((URIRef(organization.id), FOAF.homepage,
-               Literal(organization.url)))
+                   Literal(organization.url)))
         return graph
 
-    def add_topics_triples(self, graph):
+    def _add_topics_triples(self, graph):
         for ind in self.parser.get_simple_indicators():
             self._add_topic(graph, ind)
         return graph
@@ -150,19 +145,19 @@ class ReceiverRDFService(object):
     @staticmethod
     def _add_topic(graph, indicator):
         from create_db import MetadataPopulator
+
         topic_id = indicator.topic_id
         topic_label = ""
         for tp in MetadataPopulator.get_topics():
             if topic_id == tp.id:
                 topic_label = tp.translations[0].name
-
         graph.add((prefix_.term(topic_id), RDF.type,
                    lb.term("Topic")))
         graph.add((prefix_.term(topic_id), RDFS.label,
                    Literal(topic_label, lang='en')))
         return topic_id
 
-    def add_area_triples_from_slices(self, graph):
+    def _add_area_triples_from_slices(self, graph):
         slices = self.parser.get_slices()
         for slc in slices:
             if slc.country_code:
@@ -171,7 +166,7 @@ class ReceiverRDFService(object):
                 self._add_region(graph, slc)
         return graph
 
-    def add_area_triples_from_observations(self, graph):
+    def _add_area_triples_from_observations(self, graph):
         observations = self.parser.get_observations()
         for obs in observations:
             if obs.country_code:
@@ -196,7 +191,6 @@ class ReceiverRDFService(object):
                 iso2 = country.iso2
                 fao_uri = country.faoURI
                 region = country.is_part_of_id
-
         graph.add((prefix_.term(country_id), RDF.type,
                    cex.term("Area")))
         graph.add((prefix_.term(country_id), RDFS.label,
@@ -211,7 +205,7 @@ class ReceiverRDFService(object):
                    Literal(region)))
         return code
 
-    def add_region_triples(self, graph):
+    def _add_region_triples(self, graph):
         self._add_region(graph, None)
 
     @staticmethod
@@ -222,10 +216,10 @@ class ReceiverRDFService(object):
             graph.add((prefix_.term(region_id), RDF.type,
                        cex.term("Area")))
             graph.add((prefix_.term(region_id), RDFS.label,
-                      Literal(region_id, lang="en")))
+                       Literal(region_id, lang="en")))
             un_code = None
             if region_id == "Americas":
-                    un_code = 19
+                un_code = 19
             elif region_id == "Europe":
                 un_code = 150
             elif region_id == "Oceania":
@@ -246,7 +240,7 @@ class ReceiverRDFService(object):
             area = arg.region_code
         return area
 
-    def add_licenses_triples(self, graph):
+    def _add_licenses_triples(self, graph):
         lic = self.parser.get_license()
         graph.add((URIRef(lic.url), RDF.type,
                    lb.term("License")))
@@ -260,7 +254,7 @@ class ReceiverRDFService(object):
                    Literal(lic.republish, datatype=XSD.Boolean)))
         return graph
 
-    def add_computation_triples(self, graph):
+    def _add_computation_triples(self, graph):
         for obs in self.parser.get_observations():
             graph.add((cex.term(obs.computation.uri), RDF.type,
                        cex.term(Literal("Computation"))))
@@ -268,7 +262,7 @@ class ReceiverRDFService(object):
                        Literal(obs.computation.description, lang='en')))
         return graph
 
-    def add_dataset_triples(self, graph):
+    def _add_dataset_triples(self, graph):
         dataset = self.parser.get_dataset()
         lic = self.parser.get_license()
         dsource = self.parser.get_datasource()
@@ -282,21 +276,21 @@ class ReceiverRDFService(object):
                    Literal(dsource.dsource_id)))
         return graph
 
-    def add_data_source_triples(self, graph):
-        dsource = self.parser.get_datasource()
-        org = self.parser.get_organization()
+    def _add_data_source_triples(self, graph):
+        data_source = self.parser.get_datasource()
+        organization = self.parser.get_organization()
         user = self.parser.get_user()
-        graph.add((prefix_.term(dsource.dsource_id), RDF.type,
+        graph.add((prefix_.term(data_source.dsource_id), RDF.type,
                    lb.term(Literal("DataSource"))))
-        graph.add((prefix_.term(dsource.dsource_id), RDFS.label,
-                   Literal(dsource.name, lang='en')))
-        graph.add((prefix_.term(dsource.dsource_id), lb.term("organization"),
-                   URIRef(org.url)))
-        graph.add((prefix_.term(dsource.dsource_id), dcterms.term("creator"),
+        graph.add((prefix_.term(data_source.dsource_id), RDFS.label,
+                   Literal(data_source.name, lang='en')))
+        graph.add((prefix_.term(data_source.dsource_id), lb.term("organization"),
+                   URIRef(organization.url)))
+        graph.add((prefix_.term(data_source.dsource_id), dcterms.term("creator"),
                    Literal(user.id)))
         return graph
 
-    def add_upload_triples(self, graph):
+    def _add_upload_triples(self, graph):
         upload = "upload" + str(dt.datetime.now().
                                 strftime("%y%m%d%H%M"))
         user = self.parser.get_user()
@@ -313,25 +307,26 @@ class ReceiverRDFService(object):
                    Literal("156.35.82.103")))
         for obs in observations:
             graph.add((prefix_.term(upload), lb.term("observation"),
-                   prefix_.term(obs.id)))
+                       prefix_.term(obs.id)))
         graph.add((prefix_.term(upload), lb.term("dataSource"),
                    lb.term(dsource.dsource_id)))
 
         return graph
 
     @staticmethod
-    def serialize_rdf_xml(graph):
+    def _serialize_rdf_xml(graph):
         serialized = graph.serialize(format='application/rdf+xml')
         with open('../datasets/dataset.rdf', 'w') as dataset:
             dataset.write(serialized)
 
     @staticmethod
-    def serialize_turtle(graph):
+    def _serialize_turtle(graph):
         serialized = graph.serialize(format='turtle')
         with open('../datasets/dataset.ttl', 'w') as dataset:
             dataset.write(serialized)
 
-    def load_data_set(self, host, api, graph_uri):
-        sh.curl(host+api+graph_uri,
-            digest=True, u="dba:root", verbose=True, X="POST",
-            T="../datasets/dataset.ttl")
+    @staticmethod
+    def _load_data_set(host, api, graph_uri):
+        sh.curl(host + api + graph_uri,
+                digest=True, u=config.DBA_USER + ":" + config.DBA_PASSWORD,
+                verbose=True, X="POST", T="../datasets/dataset.ttl")
