b5b60b9bbbd0e9e0d5f1e44d2ab50f7686104248
Carlos Tejo <carlos.tejo@landportal.info>
2016-03-02 10:22:59 +0000

    fixed triple generation (qb:observation instead of qb:slice)

--- app/rdf_service.py
+++ app/rdf_service.py
@@ -330,7 +330,7 @@ class ReceiverRDFService(object):
                        base_dataset.term(obs.dataset_id)))
 
 	    if obs.slice_id is not None:
-	       graph.add((base_obs.term(obs.id), qb.term("slice"), base.term(str(obs.slice_id))))
+	       graph.add((base.term(str(obs.slice_id)), qb.term("observation"), base_obs.term(obs.id)))
 
             graph.add((base_obs.term(obs.id), RDFS.label,
                        Literal("Observation of " + str(region) +
